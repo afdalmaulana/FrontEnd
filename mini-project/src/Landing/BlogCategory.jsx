bdf46060d8b2c532957e5bc24fb969f3b6d0178c
afdalmaulana <afdalmaulanaaa@gmail.com>
2023-06-20 09:56:15 +0700

    update

--- mini-project/src/Landing/BlogCategory.jsx
+++ mini-project/src/Landing/BlogCategory.jsx
@@ -1,12 +1,85 @@
-import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
 import Navbar from "../Components/navbar/Navbar";
 
-export default function BlogCategory(){
-    return (
-        <>
+export default function BlogCategory() {
+  return (
+    <>
       <Box>
-        <Navbar/>
-        <Tabs mt={'100px'}>
+        <Navbar />
+        <Box mt={"100px"}>
+          <Stack>
+            <Text fontSize={"4xl"} ml={"100px"} fontWeight={"bold"}>
+              Category
+            </Text>
+            <Flex wrap={'wrap'}>
+              <Card maxW="sm" mt={"80px"} ml={"100px"} shadow={"lg"}>
+                <CardBody>
+                  <Image
+                    src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                    alt="Green double couch with wooden legs"
+                    borderRadius="lg"
+                  />
+                  <Stack mt="6" spacing="3">
+                    <Heading size="lg">Technology</Heading>
+                  </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <Button variant="solid" colorScheme="blue">
+                      Open
+                    </Button>
+                    <Button variant="ghost" colorScheme="blue">
+                      Add to cart
+                    </Button>
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
+              <Card maxW="sm" mt={"80px"} ml={"100px"} shadow={"lg"}>
+                <CardBody>
+                  <Image
+                    src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                    alt="Green double couch with wooden legs"
+                    borderRadius="lg"
+                  />
+                  <Stack mt="6" spacing="3">
+                    <Heading size="lg">Lifestyle</Heading>
+                  </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <Button variant="solid" colorScheme="blue">
+                      Open
+                    </Button>
+                    <Button variant="ghost" colorScheme="blue">
+                      Add to cart
+                    </Button>
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
+            </Flex>
+          </Stack>
+        </Box>
+        <Tabs mt={"100px"}>
           <TabList>
             <Tab>Technology</Tab>
             <Tab>Lifestyle</Tab>
