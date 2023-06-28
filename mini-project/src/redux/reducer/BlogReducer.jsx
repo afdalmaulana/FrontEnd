fb75b06dcc940acac39aaf17f1d8dafb6ae828b1
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2023-06-28 13:49:23 +0700

    create blog

--- mini-project/src/redux/reducer/BlogReducer.jsx
+++ mini-project/src/redux/reducer/BlogReducer.jsx
@@ -1,18 +1,21 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-    blog : [],
-}
-
+  blog: [],
+  newArticle: [],
+};
 
 export const BlogReducer = createSlice({
-    name: 'BlogReducer',
-    initialState,
-    reducers:{
-        addToBookmark : (state, action) => {
-            state.blog.push(action.payload)
-        }
-    }
-})
+  name: "BlogReducer",
+  initialState,
+  reducers: {
+    addToBookmark: (state, action) => {
+      state.blog.push(action.payload);
+    },
+    getArticle: (state, action) => {
+      state.newArticle = [...state.newArticle, ...action.payload];
+    },
+  },
+});
 
 export const { addToBookmark } = BlogReducer.actions;
 export default BlogReducer.reducer;
